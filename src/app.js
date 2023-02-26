79d2fdb534d780e372927de5cba47ee2ca6bb38a
richardCorreaDev <richard.correa.github@gmail.com>
2023-02-26 12:39:13 -0300

    EntregableHandlebars

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 import __dirname from './util.js';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
-
+import viewsRouter from './routes/views.router.js';
 
 //crea-inicializa el servidor
 const app = express();
 
+app.engine('handlebars', handlebars.engine())
+app.set('views',`${__dirname}/views`)
+app.set('view engine', 'handlebars')
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(`${__dirname}`));
-
+// app.use(express.static(`${__dirname}`));
+app.use('/', viewsRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
---
+++ src/routes/views.router.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import ProductManager from '../managers/ManagerProductos.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const router = Router()
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const productManager = new ProductManager(path.join(dirname, 'productos.json'));
+
+router.get('/', async (req, res) => {
+    const products = await productManager.getProducts();
+res.render('home', { products: products })
+})
+
+export default router
+
+
